2ae1d5cf212a7e10702cd8cb6d0af8ff55c12d6c
Renzo Navarro <rcnavarrop@gmail.com>
2022-07-27 21:47:08 -0400

    setState with object or function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -7,14 +7,17 @@ import * as React from 'react'
 // The 1st argument is called "state" - the current value of count
 // The 2nd argument is called "newState" - the value passed to setCount
 function countReducer(state, action) {
-  return ({...state, ...action})
+  return {...state, ...(typeof action === 'function' ? action(state) : action)}
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {count: initialCount})
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  const {count} = state;
+  const {count} = state
   const increment = () => setState({count: count + step})
+  // setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
